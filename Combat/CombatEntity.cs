5271c9ed546048c6c3ff5358b1d503a48837a705
Ara Alexanian-Farr <Alexanar@users.noreply.github.com>
2016-10-29 17:49:58 -0400

    Add files via upload

---
+++ Combat/CombatEntity.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class CombatEntity : ICombat
+{
+    public Attributes attributes;
+    public MagicDictionary spellBook;
+    public ItemDictionary lootTable;
+    //combat variables
+    public CombatManager combatManager;
+    public static Random random = new Random();
+    public int randomNumber;
+    private bool isTurn;
+    public bool dead { get; set; }
+    public int experienceWorth { get; set; }
+    //coming out of combat
+    public bool cameOutOfCombat { get; set; }
+
+    public CombatEntity(CombatManager manager, ItemDictionary inventory)
+    {
+        //attributes = new Attributes(1, 1, 1, 1, 1);
+        combatManager = manager;
+        lootTable = inventory;
+    }
+
+    //adds entity to manager and sorts
+    public virtual void AddToManager()
+    {
+        attributes.AttributeUpdate();
+        combatManager.combatEntities.Add(this);
+        combatManager.enemyList.Add(this);
+    }
+    public virtual void ItemDrops()
+    {
+
+    }
+    public int GetSpeed()
+    {
+        return attributes.turnSpeed;
+    }
+    public bool SetTurn(bool turn)
+    {
+        isTurn = turn;
+        return isTurn;
+    }
+    public bool GetTurn()
+    {
+        return isTurn;
+    }
+    public void EndTurn()
+    {
+        isTurn = false;
+        combatManager.SetupTurns();
+    }
+
+
+    public abstract void DoTurn();
+
+    //damage calc
+    public static float DamageAmount(float min, float max, int attribute, float multiplier)
+    {
+        int rand = random.Next((int)Math.Round(min), (int)Math.Round(max));
+        float damage = (rand + (attribute * multiplier));
+        return damage;
+    }
+
+    //*********************************
+    // COMBAT INTERFACE IMPLEMENTATION
+    //*********************************
+
+    public abstract void Attack();
+
+    public abstract void Magic();
+
+    public abstract void Item();
+
+    public abstract void Defend();
+
+    public string GetName()
+    {
+        return attributes.name;
+    }
+
+    public virtual void TakeDamage(float damage)
+    {
+        attributes.healthVar -= (int)Math.Round(damage);
+    }
+
+}
+
---
+++ Combat/Player.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Player : CombatEntity
+{
+
+    //text input
+    private string enteredText;
+    private List<string> availableTargets = new List<string> { "1", "2", "3", "4", "5", "6" };
+    private List<string> availableSpells = new List<string>();
+
+
+    enum EquipSlot
+    {
+        head,
+        body,
+        legs,
+        weapon
+    }
+
+    //setup player
+    public Player(CombatManager manager, ItemDictionary inventory) : base(manager, inventory)
+    {
+        attributes = new Attributes(1, 5, 5, 5, 5);
+        attributes.SetResource();
+        combatManager = manager;
+        spellBook = new MagicDictionary(this, combatManager);
+        //spell tests
+        spellBook.Fireball();
+        spellBook.ConeOfCold();
+        spellBook.Heal();
+    }
+
+    //*********************************
+    // COMBAT INTERFACE IMPLEMENTATION
+    //*********************************
+    public override void AddToManager()
+    {
+        attributes.AttributeUpdate();
+        combatManager.combatEntities.Add(this);
+        combatManager.playerList.Add(this);
+    }
+    //add player to party
+    public void AddToParty(PlayerControl playerControl)
+    {
+        playerControl.partyList.Add(this);
+    }
+    public override void DoTurn()
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public override void Attack()
+    {
+        Console.WriteLine("Choose target");
+        enteredText = PlayerControl.ReadText(enteredText, availableTargets);
+        //minus 1 to fit in list spots
+        int enemyNumber = int.Parse(enteredText) - 1; 
+        //check if target is in eenemy list
+        if (enemyNumber <= combatManager.enemyList.Count - 1)
+        {
+            //damage
+            float damage = DamageAmount(attributes.weaponDamMin, attributes.weaponDamMax, attributes.strengthTotal, 1.5f);
+            //do damage
+            PlayerControl.ClearWriteLine(combatManager.enemyList[enemyNumber].GetName() + " was dealt " + damage + " damage by " + attributes.name);
+            combatManager.enemyList[enemyNumber].TakeDamage(damage);
+            attributes.TurnSpeedSubtract(0);
+            //end
+            EndTurn();
+        }
+        else
+        {
+            Console.WriteLine("Invalid Target");
+        }
+    }
+
+    public override void Magic()
+    {
+        availableSpells = spellBook.spellDictionary.Keys.ToList();
+        //check if list has any spells
+        if (availableSpells.Any())
+        {
+            //spell list
+            Console.WriteLine("Known Spells: ");
+            foreach (string spell in availableSpells)
+            {
+                Console.WriteLine(spell);
+            }
+            Console.WriteLine("Choose spell");
+            enteredText = PlayerControl.ReadText(enteredText, availableSpells);
+            enteredText.ToLower();
+            spellBook.spellDictionary[enteredText].DynamicInvoke();
+        }
+        else
+        {
+            Console.WriteLine("You don't know any magic!");
+        }
+    }
+
+    public override void Item()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Defend()
+    {
+        throw new NotImplementedException();
+    }
+
+
+
+
+}
+
